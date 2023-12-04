d2b22a1b58e60fac66f2db3c6786e6209feb6e73
talent-oleksii <oleksiikaravanov@gmail.com>
2023-12-04 06:12:37 +0200

    change design

--- src/pages/billing/index.tsx
+++ src/pages/billing/index.tsx
@@ -45,15 +45,15 @@ const Billing: FC = () => {
       animate="show"
       variants={FADE_UP_ANIMATION_VARIANTS}
     >
-      <h2 className='text-[24px] 2xl:text-[30px] font-[Inter] text-black font-semibold -tracking-[1.02px]'>{`${company}'s Billings 📈`}</h2>
-      <p className='my-2 text-[#43474A] font-normal text-sm 2xl:text-md'>Access to your billing portal.</p>
+      <h2 className='text-[20px] 2xl:text-[24px] font-[Inter] text-black font-semibold -tracking-[.6px]'>{`${company}'s Billings 📈`}</h2>
+      <p className='mt-[6px] text-[#43474A] font-normal text-sm'>Access to your billing portal.</p>
 
-      <div className='bg-white flex items-center justify-between mt-4 p-5 2xl:p-10 rounded-[10px] shadow-md'>
+      <div className='bg-white flex items-center justify-between mt-[30px] px-[64px] py-[50px] rounded-[10px] shadow-md'>
         <div>
-          <h2 className='font-[Inter] text-black font-semibold text-[22px] 2xl:text-[25px]'>Invoices</h2>
-          <p className='font-[Inter] text-[#43474a] font-medium text-sm 2xl:text-md'>Access to billing portal to view and manage your payments</p>
+          <h2 className='font-[Inter] text-black font-semibold text-[18px] 2xl:text-[22px]'>Invoices</h2>
+          <p className='font-[Inter] text-[#43474a] font-medium mt-[22px] text-sm 2xl:text-md'>Access to billing portal to view and manage your payments</p>
         </div>
-        <button className='px-4 py-2 font-[Inter] rounded-full bg-black text-white text-sm 2xl:text-md' onClick={handleView}>View billing portal</button>
+        <button className='px-4 py-2 font-[Inter] rounded-[5px] bg-black text-white text-sm 2xl:text-md font-medium' onClick={handleView}>View billing portal</button>
       </div>
     </motion.div>
   );
--- src/pages/dashboard/CampaignDetail.tsx
+++ src/pages/dashboard/CampaignDetail.tsx
@@ -29,47 +29,47 @@ const CampaignDetail: FC<typeCampaignDetail> = ({ id }: typeCampaignDetail) => {
 
   return (
     <div className='relative'>
-      {loading && <Loading />}
+      {/* {loading && <Loading />} */}
       {!loading && data && <>
-        <div className='mt-3 rounded-[10px] grid grid-cols-4 gap-4'>
-          <div className='col-span-1 py-5 px-4 flex flex-col justify-center items-center rounded-[20px] bg-white'>
-            <h2 className='text-[20px] 2xl:text-[25px] font-[Inter] font-semibold'>0</h2>
-            <p className='text-xs font-[Inter] font-semibold my-1 text-gray-600'>Total Impressions</p>
-            <div className='bg-[#7ffbae] rounded-full font-[Inter] py-1 px-4 text-xs font-semibold text-black my-1'>0%</div>
-            <p className='text-gray-500 text-[10px]'>from 0 (last 4 weeks)</p>
+        <div className='mt-[11px] rounded-[10px] grid grid-cols-4 gap-[16px]'>
+          <div className='col-span-1 pt-[25px] pb-[20px] flex flex-col justify-center items-center rounded-[20px] bg-white shadow-md'>
+            <h2 className='text-[25px] 2xl:text-[28px] font-[Inter] font-semibold'>0</h2>
+            <p className='text-[10px] 2xl:text-xs font-[Inter] font-normal mt-[5px] text-[#43474A]'>Total Impressions</p>
+            <div className='bg-[#7ffbae] rounded-full mt-[12px] font-[Inter] py-[1px] px-[10px] text-[10px] 2xl:text-xs font-semibold text-black my-1'>0%</div>
+            <p className='text-[#7F8182] text-[8px] mt-[5px] 2xl:text-[10px] font-semibold'>from 0 (last 4 weeks)</p>
           </div>
           <div className='col-span-1 py-5 px-4 flex flex-col justify-center items-center items-center rounded-[20px] bg-white'>
-            <h2 className='text-[20px] 2xl:text-[25px] font-[Inter] font-semibold'>{data.click_count}</h2>
-            <p className='text-xs font-[Inter] font-normal my-1 text-gray-600'>Total Clicks</p>
-            <div className='bg-[#7ffbae] rounded-full font-[Inter] py-1 px-4 text-xs font-semibold text-black my-1'>0%</div>
-            <p className='text-gray-500 text-[10px]'>from 0 (last 4 weeks)</p>
+            <h2 className='text-[25px] 2xl:text-[28px] font-[Inter] font-semibold'>{data.click_count}</h2>
+            <p className='text-[10px] 2xl:text-xs font-[Inter] font-normal mt-[5px] text-[#43474A]'>Total Clicks</p>
+            <div className='bg-[#7ffbae] rounded-full mt-[12px] font-[Inter] py-[1px] px-[10px] text-[10px] 2xl:text-xs font-semibold text-black my-1'>0%</div>
+            <p className='text-[#7F8182] text-[8px] mt-[5px] 2xl:text-[10px] font-semibold'>from 0 (last 4 weeks)</p>
           </div>
           <div className='col-span-1 py-5 px-4 flex flex-col justify-center items-center items-center rounded-[20px] bg-white'>
-            <h2 className='text-[20px] 2xl:text-[25px] font-[Inter] font-semibold'>{`$${Number(data.click_count) * (data.demographic === 'consumer' ? 8 : 20)}`}</h2>
-            <p className='text-xs font-[Inter] font-normal my-1 text-gray-600'>Total Spend</p>
-            <div className='bg-[#7ffbae] rounded-full font-[Inter] py-1 px-4 text-xs font-semibold text-black my-1'>0%</div>
-            <p className='text-gray-500 text-[10px]'>from $12,000 (last 4 weeks)</p>
+            <h2 className='text-[25px] 2xl:text-[28px] font-[Inter] font-semibold'>{`$${Number(data.click_count) * (data.demographic === 'consumer' ? 8 : 20)}`}</h2>
+            <p className='text-[10px] 2xl:text-xs font-[Inter] font-normal mt-[5px] text-[#43474A]'>Total Spend</p>
+            <div className='bg-[#7ffbae] rounded-full mt-[12px] font-[Inter] py-[1px] px-[10px] text-[10px] 2xl:text-xs font-semibold text-black my-1'>0%</div>
+            <p className='text-[#7F8182] text-[8px] mt-[5px] 2xl:text-[10px] font-semibold'>from $12,000 (last 4 weeks)</p>
           </div>
           <div className='col-span-1 py-5 px-4 flex flex-col justify-center items-center items-center rounded-[20px] bg-white'>
-            <h2 className='text-[20px] 2xl:text-[25px] font-[Inter] font-semibold'>{`$${data.demographic === 'consumer' ? 8 : 20}`}</h2>
-            <p className='text-xs font-[Inter] font-normal my-1 text-gray-600'>AVG CPC</p>
-            <div className='bg-[#7ffbae] rounded-full font-[Inter] py-1 px-4 text-xs font-semibold text-black my-1'>- 20%</div>
-            <p className='text-gray-500 text-[10px]'>from $10 (last 4 weeks)</p>
+            <h2 className='text-[25px] 2xl:text-[28px] font-[Inter] font-semibold'>{`$${data.demographic === 'consumer' ? 8 : 20}`}</h2>
+            <p className='text-[10px] 2xl:text-xs font-[Inter] font-normal mt-[5px] text-[#43474A]'>AVG CPC</p>
+            <div className='bg-[#7ffbae] rounded-full mt-[12px] font-[Inter] py-[1px] px-[10px] text-[10px] 2xl:text-xs font-semibold text-black my-1'>- 20%</div>
+            <p className='text-[#7F8182] text-[8px] mt-[5px] 2xl:text-[10px] font-semibold'>from $10 (last 4 weeks)</p>
           </div>
         </div>
 
-        <div className='my-4 p-5 min-h-[250px] rounded-[10px] bg-white'>
+        <div className='my-[14px] p-[25px] min-h-[250px] rounded-[10px] bg-white shadow-md'>
           <div className='flex justify-between items-center'>
             <div>
-              <h2 className='font-[Inter] text-[18px] 2xl:text-[20px] font-semibold'>
+              <h2 className='font-[Inter] text-base 2xl:text-lg font-semibold'>
                 {data.name}
               </h2>
 
               <span className='text-xs'>{`https://presspool-frontend.onrender.com/#/cul/${data.uid}`}</span>
-              <p className='font-[Inter] text-gray-500 text-xs 2xl:text-sm'>Let's see how your campaign is performing</p>
+              <p className='font-[Inter] text-[#43474A] mt-[5px] text-xs 2xl:text-sm'>Let's see how your campaign is performing</p>
 
             </div>
-            <button className='border-[1px] px-2 py-1 font-[Inter] rounded-[5px] text-sm 2xl:text-md font-semibold border-[#7f8182]'>
+            <button className='border-[1px] px-2 py-1 font-[Inter] rounded-[5px] text-xs 2xl:text-sm font-semibold border-[#7f8182]'>
               Download as PDF
             </button>
           </div>
@@ -83,8 +83,8 @@ const CampaignDetail: FC<typeCampaignDetail> = ({ id }: typeCampaignDetail) => {
               <YAxis />
             </LineChart>
             <div className='p-4 absolute right-[20px] top-0'>
-              <p className='font-[Inter] text-black text-xs 2xl:text-sm mb-2'>Total Impressions</p>
-              <p className='font-[Inter] text-[#7FFBAE] text-xs 2xl:text-sm mt-2'>Total Clicks</p>
+              <p className='font-[Inter] text-black text-xs 2xl:text-sm font-semibold mb-2'>Total Impressions</p>
+              <p className='font-[Inter] text-[#7F8182] text-xs 2xl:text-sm mt-2 font-semibold'>Total Clicks</p>
             </div>
           </div>
         </div>
@@ -144,6 +144,7 @@ const CampaignDetail: FC<typeCampaignDetail> = ({ id }: typeCampaignDetail) => {
 
             </tbody>
           </table>
+          <p className='font-[Inter] mt-4 text-sm'>No data is available. Please create and launch your first campaign</p>
         </div>
       </>
       }
