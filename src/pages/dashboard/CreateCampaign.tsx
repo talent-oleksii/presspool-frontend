ad655ff4d1d9d47f570945ba79d396a64a6f3a84
talent-oleksii <oleksiikaravanov@gmail.com>
2024-01-05 18:24:30 +0200

    add new comments for budget tab on create/edit page

--- src/pages/dashboard/CreateCampaign.tsx
+++ src/pages/dashboard/CreateCampaign.tsx
@@ -35,7 +35,7 @@ const CreateCampaign: FC = () => {
   const [currentTab, setCurrentTab] = useState('detail');
   const [campaignName, setCampaignName] = useState('');
   const [currentTarget, setCurrentTarget] = useState('consumer');
-  const [currentPrice, setCurrentPrice] = useState('20000');
+  const [currentPrice, setCurrentPrice] = useState('10000');
   const [audience, setAudience] = useState<Array<any>>([]);
   const [currentAudience, setCurrentAudience] = useState<Array<any>>([]);
   const [uiData, setUiData] = useState<any>(undefined);
@@ -160,7 +160,7 @@ const CreateCampaign: FC = () => {
         setCurrentTab('detail');
         setCampaignName('');
         setCurrentTarget('consumer');
-        setCurrentPrice('20000');
+        setCurrentPrice('10000');
         setUiData(undefined);
         setUrl('');
         setCurrentAudience([]);
@@ -567,7 +567,9 @@ const CreateCampaign: FC = () => {
               className='w-[720px]'
             >
               <h2 className='font-[Inter] text-md 2xl:text-lg font-semibold text-black'>Please type in your budget cap for this campaign</h2>
-              <p className='font-[Inter] text-gray-600 text-xs 2xl:text-sm mt-[14px]'>*Keep in mind, these are all verified, targeted, engaged and trusting readers that will be clicking through directly to your landing page of choice</p>
+              <p className='font-[Inter] text-[#43474a] text-xs 2xl:text-sm mt-[14px]'>
+                *Keep in mind, these are all verified, targeted and engaged readers that will be clicking through directly to your landing page of choice. We only charge per <span className='font-bold'>unique click</span> as they come in.
+              </p>
               <div className='pl-2 pr-4 py-1 mt-[23px] border-[1px] rounded-lg border-black w-full flex justify-between items-center relative'>
                 <input
                   value={currentPrice}
@@ -575,14 +577,28 @@ const CreateCampaign: FC = () => {
                   className='border-0 focus:border-0 focus:ring-0 focus-visible:outline-0 focus-visible:border-0 flex-1 text-sm 2xl:text-md'
                   onChange={e => setCurrentPrice(e.target.value)}
                   type="number"
-                  min="20000"
+                  min="10000"
                 />
                 <p className='text-[#f76363] font-normal text-xs right-[23px] top-[10px] ms-2'>*minimum input must be $10,000</p>
               </div>
               {currentPrice &&
                 <div className='mt-[9px]'>
-                  <span className='font-[Inter] text-xs 2xl:text-sm my-3 text-[#6c63ff]'>
+                  <span className='font-[Inter] text-xs 2xl:text-sm my-3 text-black'>
                     {`*Estimated clicks for the campaign are ${Math.floor(Number(currentPrice) / (currentTarget === 'consumer' ? 8 : 20))}`}
+                  </span>
+                </div>
+              }
+              {
+                Number(currentPrice) < 20000 &&
+                <div className='mt-4 bg-[#fcd9d7] rounded-lg p-2 flex'>
+                  <div>
+                    <svg enableBackground="new 0 0 91.8 92.6" version="1.0" viewBox="0 0 91.8 92.6" xmlns="http://www.w3.org/2000/svg" className='w-[16px] me-1'>
+                      <path d="M45.9,3.6c-23.5,0-42.5,19-42.5,42.5c0,23.5,19,42.5,42.5,42.5c23.5,0,42.5-19,42.5-42.5  C88.4,22.7,69.4,3.6,45.9,3.6z M43.7,21.1h4.3c0.5,0,0.9,0.4,0.9,0.9l-0.6,34.5c0,0.5-0.4,0.9-0.9,0.9h-3c-0.5,0-0.9-0.4-0.9-0.9  L42.8,22C42.8,21.5,43.2,21.1,43.7,21.1z M48.6,71.2c-0.8,0.8-1.7,1.1-2.7,1.1c-1,0-1.9-0.3-2.6-1c-0.8-0.7-1.3-1.8-1.3-2.9  c0-1,0.4-1.9,1.1-2.7c0.7-0.8,1.8-1.2,2.9-1.2c1.2,0,2.2,0.5,3,1.4c0.5,0.6,0.8,1.3,0.9,2.1C49.9,69.3,49.5,70.3,48.6,71.2z" fill="#525252" />
+                    </svg>
+                  </div>
+
+                  <span className='text-[#525252] text-xs font-medium'>
+                    We recommend setting your budget to $20,000 or more to ensure top-tier newsletter placements—highest bidders receive the best spots. This also accelerates data collection, enabling quicker campaign optimization and enhanced results.
                   </span>
                 </div>
               }
@@ -590,7 +606,7 @@ const CreateCampaign: FC = () => {
                 <button
                   className='rounded-[5px] bg-[#7FFBAE] px-[50px] py-[10px] text-black font-semibold disabled:bg-gray-400 text-sm'
                   onClick={handleBeforeReview}
-                  disabled={Number(currentPrice) < 20000}
+                  disabled={Number(currentPrice) < 10000}
                 >
                   Next Step
                 </button>
@@ -694,7 +710,7 @@ const CreateCampaign: FC = () => {
               </div>
               <div className='w-full text-center mt-[50px]'>
                 {
-                  currentAudience.length >= 1 && Number(currentPrice) >= 20000 &&
+                  currentAudience.length >= 1 && Number(currentPrice) >= 10000 &&
                   <button className='rounded-[5px] text-black bg-[#7FFBAE] px-[50px] 2xl:px-[60px] py-[10px] font-semibold mt-2 disabled:bg-gray-300 text-sm 2xl:text-md'
                     disabled={!isSubmitable()}
                     onClick={handleSubmit}
