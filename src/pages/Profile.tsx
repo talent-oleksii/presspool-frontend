51ec2d7a94cf3b7653659227c272fcd3ae5e729f
Sahil Gupta <sahilhgupta562@gmail.com>
2024-02-23 03:46:57 +0530

    Fixed account layout and remove card functionality

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -191,6 +191,23 @@ const Profile: FC = () => {
     handleClose();
   };
 
+  const handleRemoveCard = async (customerId: string, cardId: string) => {
+    setLoading(true);
+    const { data } = await StripeUtil.deleteCard(customerId, cardId);
+    dispatch(
+      setCardList({
+        cardList: data.map((item) => ({
+          create_time: item.created,
+          card_id: item.id,
+          last4: item.card?.last4,
+          brand: item.card?.brand,
+          customer_id: item.customer,
+        })),
+      })
+    );
+    setLoading(false);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between pr-4 pt-1.5">
@@ -342,8 +359,20 @@ const Profile: FC = () => {
                       {/* <div className='flex justify-between w-full'>
                       <p className='font-[Inter] text-[#7f8182] text-xs -tracking-[.42px]'>{`Added Date: ${moment.unix(Number(item.create_time)).format('DD MMM, yyyy')}`}</p>
                     </div> */}
-                      <div className="bg-[#fbfbfb] text-base text-black border-[1px] border-[rgba(127, 129, 130, 0.13)] rounded-[10px] p-3 my-1.5">{`${item.brand.toUpperCase()} **** **** **** ${item.last4
-                        }`}</div>
+                      <div className="bg-[#fbfbfb] text-base text-black border-[1px] border-[rgba(127, 129, 130, 0.13)] rounded-[10px] p-3 my-1.5 flex justify-between">
+                        {`${item.brand.toUpperCase()} **** **** **** ${
+                          item.last4
+                        }`}
+                        <div
+                          role="button"
+                          className="text-[#7F8182] cursor-pointer"
+                          onClick={() =>
+                            handleRemoveCard(item.customer_id, item.card_id)
+                          }
+                        >
+                          Remove
+                        </div>
+                      </div>
                     </div>
                   ))}
                   {cardList.length <= 0 && (
@@ -466,123 +495,61 @@ const Profile: FC = () => {
                 className="col-span-1 rounded-[12px] bg-white border-[1px] border-[#7f8182] shadow-md p-4"
                 key={item.id}
               >
-                <div className="border-b-[1px] border-[#ddd] pb-5">
-                  <p className="font-[Inter] text-[10px] font-normal -tracking-[.36px] text-[#a3a3a3]">
-                    {item.role === "admin"
-                      ? "Administrator"
-                      : "Campaign Manager"}
-                  </p>
-                  <div className="flex items-center justify-between mt-2">
-                    <div className="flex">
-                      <Avatar
-                        className="bg-main text-black items-center justify-center flex"
-                        src={item.avatar}
-                        alt={getPlaceHolder(item.name)}
-                        size={40}
-                      >
-                        {(!item.avatar || item.avatar.length <= 3) && (
-                          <span className="text-xs font-[Inter] font-medium">
-                            {getPlaceHolder(item.name)}
-                          </span>
-                        )}
-                      </Avatar>
-                      <div className="text-left ms-2">
-                        <p className="font-[Inter] text-[#43474a] text-sm font-medium -tracking-[.36px]">
-                          {item.name}
-                        </p>
-                        <p className="font-[Inter] text-[#A3A3A3] text-[10px] font-normal -tracking-[.3px]">
-                          {item.manager}
-                        </p>
-                      </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex">
+                    <Avatar
+                      className="bg-main text-black items-center justify-center flex"
+                      src={item.avatar}
+                      alt={getPlaceHolder(item.name)}
+                      size={53}
+                    >
+                      {(!item.avatar || item.avatar.length <= 3) && (
+                        <span className="text-xs font-[Inter] font-medium">
+                          {getPlaceHolder(item.name)}
+                        </span>
+                      )}
+                    </Avatar>
+                    <div className="text-left ms-2 flex flex-col gap-1">
+                      <p className="font-[Inter] text-[#43474a] text-sm font-medium -tracking-[.36px] leading-[14px]">
+                        {item.name}
+                      </p>
+                      <p className="font-[Inter] text-[#A3A3A3] text-[10px] font-normal -tracking-[.3px]">
+                        {item.manager}
+                      </p>
+                      <p className="font-[Inter] text-[#A3A3A3] text-[8px] font-normal -tracking-[.3px]">
+                        {`Joined : ${moment(
+                          new Date((item.create_time / 1000) * 1000)
+                        ).format("MM/DD/YYYY")}`}
+                      </p>
                     </div>
-                    <button
-                      className="flex text-[8px] text-white px-2 py-[2px] bg-[#E3392E] rounded-[3.25px] font-[Inter] items-center justify-center"
-                      onClick={() =>
-                        setTeamData(
-                          teamData.filter((team) => team.id !== item.id)
-                        )
-                      }
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="8"
-                        height="9"
-                        viewBox="0 0 8 9"
-                        fill="none"
-                        className="me-1"
-                      >
-                        <path
-                          d="M7.02844 1.7009C7.13382 1.70102 7.23518 1.74252 7.31181 1.81692C7.38844 1.89132 7.43455 1.99301 7.44073 2.10121C7.44691 2.2094 7.41268 2.31594 7.34504 2.39905C7.2774 2.48216 7.18146 2.53557 7.07681 2.54837L7.02844 2.55135H6.99495L6.61497 7.22883C6.61499 7.55422 6.49411 7.86731 6.27705 8.10405C6.06 8.34079 5.76319 8.48329 5.44735 8.50238L5.37458 8.5045H2.06686C1.40615 8.5045 0.866163 7.9734 0.829778 7.33513L0.827711 7.26412L0.446083 2.55135H0.413006C0.307623 2.55123 0.206261 2.50973 0.129631 2.43533C0.0530011 2.36093 0.00688714 2.25924 0.000711173 2.15105C-0.00546479 2.04285 0.0287634 1.93631 0.0964021 1.8532C0.164041 1.77009 0.259985 1.71668 0.364631 1.70388L0.413006 1.7009H7.02844ZM3.10507 3.88656C3.01844 3.83352 2.91596 3.81487 2.81689 3.8341C2.71783 3.85333 2.629 3.90912 2.56713 3.99099C2.50526 4.07285 2.47461 4.17514 2.48093 4.27862C2.48726 4.3821 2.53012 4.47963 2.60147 4.55289L3.13567 5.1027L2.60147 5.65252L2.56716 5.69249C2.5029 5.77796 2.47268 5.88537 2.48264 5.99292C2.49261 6.10047 2.542 6.20009 2.62079 6.27154C2.69958 6.34299 2.80187 6.38092 2.90686 6.37762C3.01186 6.37433 3.11171 6.33005 3.18611 6.25379L3.72072 5.7044L4.25533 6.25379L4.2942 6.28908C4.3773 6.35517 4.48174 6.38624 4.58632 6.376C4.69089 6.36575 4.78776 6.31495 4.85723 6.23392C4.92671 6.15289 4.96359 6.04769 4.96038 5.93971C4.95717 5.83172 4.91412 5.72904 4.83997 5.65252L4.30577 5.1027L4.83997 4.55289L4.87429 4.51292C4.93854 4.42745 4.96876 4.32003 4.9588 4.21248C4.94884 4.10493 4.89944 4.00532 4.82065 3.93386C4.74186 3.86241 4.63958 3.82448 4.53458 3.82778C4.42958 3.83108 4.32974 3.87535 4.25533 3.95162L3.72072 4.50101L3.18611 3.95162L3.14725 3.91632L3.10507 3.88656Z"
-                          fill="white"
-                        />
-                        <path
-                          d="M4.5473 0C4.76662 0 4.97695 0.0896007 5.13203 0.249091C5.28711 0.408581 5.37423 0.624897 5.37423 0.85045C5.37411 0.958832 5.33376 1.06308 5.26142 1.14189C5.18907 1.2207 5.0902 1.26812 4.985 1.27447C4.87979 1.28082 4.7762 1.24562 4.69539 1.17606C4.61458 1.1065 4.56264 1.00782 4.55019 0.900202L4.5473 0.85045H2.89344L2.89055 0.900202C2.8781 1.00782 2.82617 1.1065 2.74535 1.17606C2.66454 1.24562 2.56095 1.28082 2.45575 1.27447C2.35054 1.26812 2.25167 1.2207 2.17933 1.14189C2.10698 1.06308 2.06663 0.958832 2.06651 0.85045C2.06645 0.635892 2.14524 0.429237 2.2871 0.271911C2.42895 0.114586 2.62339 0.0182181 2.83142 0.00212617L2.89344 0H4.5473Z"
-                          fill="white"
-                        />
-                      </svg>
-                      Remove
-                    </button>
                   </div>
-                </div>
-                <div className="grid grid-cols gap-4 mt-5">
-                  <div className="flex flex-col gap-3">
-                    <p className="font-[Inter] text-xs font-medium -tracking-[.36px] text-[#a3a3a3]">
-                      Access Level
-                    </p>
-                    <select
-                      className="rounded-[5px] w-full border-[1px] border-black text-[10px] font-medium h-[25px] py-0"
-                      value={item.role}
-                      onChange={(e) => {
-                        const value = e.target.value;
-                        setTeamData(
-                          teamData.map((team) => {
-                            if (team.id === item.id)
-                              return { ...team, role: value };
-                            return team;
-                          })
-                        );
-                      }}
+                  <button
+                    className="flex text-[8px] text-white px-2 py-[2px] bg-[#E3392E] h-[24px] rounded-[3.25px] font-[Inter] items-center justify-center"
+                    onClick={() =>
+                      setTeamData(
+                        teamData.filter((team) => team.id !== item.id)
+                      )
+                    }
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="8"
+                      height="9"
+                      viewBox="0 0 8 9"
+                      fill="none"
+                      className="me-1"
                     >
-                      <option value="admin">Admin</option>
-                      <option value="manager">Campaign Manager</option>
-                    </select>
-                  </div>
-                  {/* <div className="flex flex-col gap-3">
-                    <p className="font-[Inter] text-xs font-medium -tracking-[.36px] text-[#a3a3a3]">
-                      Campaigns Assigned
-                    </p>
-                    <Select
-                      mode="multiple"
-                      disabled={item.role === "admin"}
-                      className="w-full h-[25px] rounded-[5px]"
-                      value={campaign
-                        .filter((y) =>
-                          item.campaign_list
-                            .split(",")
-                            .includes(y.id.toString())
-                        )
-                        .map((x) => x.id)}
-                      onChange={(e) => {
-                        console.log(e);
-                        setTeamData(
-                          teamData.map((team) => {
-                            if (team.id === item.id)
-                              return { ...team, campaign_list: e.join(",") };
-                            return team;
-                          })
-                        );
-                      }}
-                      placeholder={<p className="text-xs">Select Campaigns</p>}
-                      options={campaign.map((c) => ({
-                        value: c.id,
-                        label: (
-                          <p className="text-black text-xs font-[Inter]">
-                            {c.name}
-                          </p>
-                        ),
-                      }))}
-                    />
-                  </div> */}
+                      <path
+                        d="M7.02844 1.7009C7.13382 1.70102 7.23518 1.74252 7.31181 1.81692C7.38844 1.89132 7.43455 1.99301 7.44073 2.10121C7.44691 2.2094 7.41268 2.31594 7.34504 2.39905C7.2774 2.48216 7.18146 2.53557 7.07681 2.54837L7.02844 2.55135H6.99495L6.61497 7.22883C6.61499 7.55422 6.49411 7.86731 6.27705 8.10405C6.06 8.34079 5.76319 8.48329 5.44735 8.50238L5.37458 8.5045H2.06686C1.40615 8.5045 0.866163 7.9734 0.829778 7.33513L0.827711 7.26412L0.446083 2.55135H0.413006C0.307623 2.55123 0.206261 2.50973 0.129631 2.43533C0.0530011 2.36093 0.00688714 2.25924 0.000711173 2.15105C-0.00546479 2.04285 0.0287634 1.93631 0.0964021 1.8532C0.164041 1.77009 0.259985 1.71668 0.364631 1.70388L0.413006 1.7009H7.02844ZM3.10507 3.88656C3.01844 3.83352 2.91596 3.81487 2.81689 3.8341C2.71783 3.85333 2.629 3.90912 2.56713 3.99099C2.50526 4.07285 2.47461 4.17514 2.48093 4.27862C2.48726 4.3821 2.53012 4.47963 2.60147 4.55289L3.13567 5.1027L2.60147 5.65252L2.56716 5.69249C2.5029 5.77796 2.47268 5.88537 2.48264 5.99292C2.49261 6.10047 2.542 6.20009 2.62079 6.27154C2.69958 6.34299 2.80187 6.38092 2.90686 6.37762C3.01186 6.37433 3.11171 6.33005 3.18611 6.25379L3.72072 5.7044L4.25533 6.25379L4.2942 6.28908C4.3773 6.35517 4.48174 6.38624 4.58632 6.376C4.69089 6.36575 4.78776 6.31495 4.85723 6.23392C4.92671 6.15289 4.96359 6.04769 4.96038 5.93971C4.95717 5.83172 4.91412 5.72904 4.83997 5.65252L4.30577 5.1027L4.83997 4.55289L4.87429 4.51292C4.93854 4.42745 4.96876 4.32003 4.9588 4.21248C4.94884 4.10493 4.89944 4.00532 4.82065 3.93386C4.74186 3.86241 4.63958 3.82448 4.53458 3.82778C4.42958 3.83108 4.32974 3.87535 4.25533 3.95162L3.72072 4.50101L3.18611 3.95162L3.14725 3.91632L3.10507 3.88656Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M4.5473 0C4.76662 0 4.97695 0.0896007 5.13203 0.249091C5.28711 0.408581 5.37423 0.624897 5.37423 0.85045C5.37411 0.958832 5.33376 1.06308 5.26142 1.14189C5.18907 1.2207 5.0902 1.26812 4.985 1.27447C4.87979 1.28082 4.7762 1.24562 4.69539 1.17606C4.61458 1.1065 4.56264 1.00782 4.55019 0.900202L4.5473 0.85045H2.89344L2.89055 0.900202C2.8781 1.00782 2.82617 1.1065 2.74535 1.17606C2.66454 1.24562 2.56095 1.28082 2.45575 1.27447C2.35054 1.26812 2.25167 1.2207 2.17933 1.14189C2.10698 1.06308 2.06663 0.958832 2.06651 0.85045C2.06645 0.635892 2.14524 0.429237 2.2871 0.271911C2.42895 0.114586 2.62339 0.0182181 2.83142 0.00212617L2.89344 0H4.5473Z"
+                        fill="white"
+                      />
+                    </svg>
+                    Remove
+                  </button>
                 </div>
               </div>
             ))}
