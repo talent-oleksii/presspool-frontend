e23cf1deb5f6dec34cb0d56c18dfb4ab698a0a0e
Joseph Goerner <goerner.joseph01@gmail.com>
2024-03-05 14:33:29 -0600

    Secondary tracking measure

--- src/pages/URLRedirector.tsx
+++ src/pages/URLRedirector.tsx
@@ -1,5 +1,7 @@
 import { FC, useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import ReactGA from 'react-ga';
 import APIInstance from '../api';
 import Loading from '../components/Loading';
 
@@ -7,9 +9,10 @@ const URLRedirector: FC = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [showText, setShowText] = useState('Redirecting');
-  useEffect(() => {
-    document.title = 'loading...';
 
+  useEffect(() => {
+    ReactGA.initialize('G-NMJTFS236X');
+    document.title = 'loading...';
     return () => {
       document.title = 'loading...';
     };
@@ -21,8 +24,21 @@ const URLRedirector: FC = () => {
         .then(data => {
           data.json().then(data => {
             const ipAddress = data.ip;
-            APIInstance.post('data/clicked', { id, ipAddress }).then(data => {
+            
+            // Generate unique click identifier
+            const uniqueClickId = uuidv4();
+            
+            // Send data to Track.presspool.ai
+            APIInstance.post('data/clicked', { id, ipAddress, uniqueClickId }).then(data => {
               window.open(data.data.url, '_self');
+              
+              // Send event to GA4
+              ReactGA.event({
+                category: 'Newsletter',
+                action: 'Click',
+                label: 'CTA',
+                dimension1: uniqueClickId,
+              });
             }).catch(err => {
               setShowText('Campaign Not Activated!');
             }).finally(() => setLoading(false));
