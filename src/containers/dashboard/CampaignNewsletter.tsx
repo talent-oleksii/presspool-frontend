ab84616620a811a485241500e811d3202cb7a271
talent-oleksii <oleksiikaravanov@gmail.com>
2024-04-23 22:59:37 +0300

    show date on newsletter field

--- src/containers/dashboard/CampaignNewsletter.tsx
+++ src/containers/dashboard/CampaignNewsletter.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { selectData } from "../../store/dataSlice";
+import moment from "moment";
 
 const CampaignNewsletter: React.FC<{ avgCPC: number }> = (props) => {
   const { avgCPC } = props;
@@ -20,6 +21,8 @@ const CampaignNewsletter: React.FC<{ avgCPC: number }> = (props) => {
       acc[name].total_clicks += parseInt(entry.total_clicks);
       acc[name].unique_clicks += parseInt(entry.unique_clicks);
       acc[name].verified_clicks += parseInt(entry.verified_clicks);
+      if (!acc[name].create_time) acc[name].create_time = entry.create_time;
+      else acc[name].create_time = acc[name].create_time > entry.create_time ? entry.create_time : acc[name].create_time;
       return acc;
     }, {});
     return Object.entries(aggregatedData).map(([name, values]: any) => ({
@@ -38,18 +41,19 @@ const CampaignNewsletter: React.FC<{ avgCPC: number }> = (props) => {
         <div className="text-center">Total Clicks</div>
         <div className="text-center">Unique Clicks</div>
         <div className="text-center">Verified Clicks</div>
-        <div className="text-center">Total Spent</div>
+        <div className="text-center">Projected Spend</div>
         {/* <div className="text-center">Rating</div> */}
       </div>
       {data.length
         ? data.map((item, index) => (
           <div
             key={index}
-            className="rounded-[10px] grid grid-cols-5 gap-3 min-h-[60px] items-end justify-center"
+            className="rounded-[10px] grid grid-cols-5 gap-3 min-h-[60px] items-center justify-center"
           >
             <div className="text-primary font-bold text-sm">
               {/* {item.name.includes('.') ? <a href={`https://${item.name}`} target="_blank" rel="noreferrer" className="text-[red]/[.74]">{item.name}</a> : <p>{item.name}</p>} */}
               {item.name}
+              <p className="font-normal text-xs">{moment(Number(item.create_time)).format('MM/DD/YYYY')}</p>
             </div>
             <div className="text-primary font-medium text-sm text-center">
               {item.total_clicks}
