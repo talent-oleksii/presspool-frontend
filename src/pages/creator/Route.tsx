ec803cce181803c98408a2cf07d98d8228c83c3d
Sahil Gupta <sahilhgupta562@gmail.com>
2024-04-30 12:06:25 +0530

    merged

--- src/pages/creator/Route.tsx
+++ src/pages/creator/Route.tsx
@@ -2,14 +2,14 @@ import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import CreatorLogin from "./login";
 import CreatorSignUp from "./signup";
 import Onboarding from "./onboarding";
-import Creator from "./Layout";
 import CreatorAuth from "../../utils/creatorauth.utils";
 import CreatorLayout from "./Layout";
 import CreatorDashboard from "./dashboard";
+import CreatorReporting from "./reporting";
 
 const GuestCreatorRoutes = () => {
   const isAuthenticated = new CreatorAuth().isAuthenticated();
-  return !isAuthenticated ? <Outlet /> : <Navigate to={"/creator/reporting"} />;
+  return !isAuthenticated ? <Outlet /> : <Navigate to={"/creator/dashboard"} />;
 };
 
 const ProtectedRouteWithoutLayout = () => {
@@ -31,7 +31,7 @@ const ProtectedRouteWithLayout = () => {
 const CreatorBaseRouteRedirection = () => {
   const isAuthenticated = new CreatorAuth().isAuthenticated();
   return isAuthenticated ? (
-    <Navigate to={"/creator/reporting"} />
+    <Navigate to={"/creator/dashboard"} />
   ) : (
     <Navigate to={`/creator/login`} />
   );
@@ -46,10 +46,11 @@ const CreatorRoute = () => {
         <Route path="/:creatorId/onboarding" element={<Onboarding />} />
       </Route>
       <Route element={<ProtectedRouteWithoutLayout />}>
-        <Route path="/*" element={<Creator />} />
+        <Route path="/*" element={<CreatorLayout />} />
       </Route>
       <Route element={<ProtectedRouteWithLayout />}>
-        <Route path="/reporting/:id" element={<CreatorDashboard />} />
+        <Route path="/dashboard" element={<CreatorDashboard />} />
+        <Route path="/reporting/:id" element={<CreatorReporting />} />
       </Route>
       <Route path="/" element={<CreatorBaseRouteRedirection />} />
     </Routes>
--- src/pages/creator/dashboard/index.tsx
+++ src/pages/creator/dashboard/index.tsx
@@ -1,84 +1,45 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  selectData,
-  setCampaign,
-  setCampaignLoading,
-  setClicked,
-  setNewsletter,
-  setPrevRangeData,
-} from "../../../store/dataSlice";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import { MAIN_ROUTE_FADE_UP_ANIMATION_VARIANTS } from "../../../utils/TransitionConstants";
-import CampaignFilter from "../../../containers/dashboard/CampaignFilter";
-import CampaignOverView from "../../dashboard/CampaignOverView";
-import Loading from "../../../components/Loading";
-import { IDateRange } from "../../../interfaces/common.interface";
-import CreatorAPIInstance from "../../../api/creatorAPIInstance";
+import NewCampaignRequests from "../../../containers/creator/dashboard/NewCampaignRequests";
+import ReadyToPublishCampaigns from "../../../containers/creator/dashboard/ReadyToPublishCampaigns";
+import ActiveCampaigns from "../../../containers/creator/dashboard/ActiveCampaigns";
+import CompletedCampaigns from "../../../containers/creator/dashboard/CompletedCampaigns";
 
 const CreatorDashboard = () => {
-  const dispatch = useDispatch();
   const [isFirstLoad, setIsFirstLoad] = useState(true);
-  const { isCampaignLoading } = useSelector(selectData);
   useEffect(() => {
     setIsFirstLoad(false);
   }, []);
+  const [selectedTab, setSelectedTab] = useState("readyToPublish");
 
-  const loadCampaignData = useCallback(
-    (
-      dateRange: IDateRange,
-      identifier: string,
-      selectedCampaigns: string[]
-    ) => {
-      dispatch(setCampaignLoading(true));
-      Promise.all([
-        CreatorAPIInstance.get("getCampaign", {
-          params: {
-            creatorId: identifier,
-            ...(dateRange.endDate &&
-              dateRange.startDate && {
-                from: dateRange.startDate,
-                to: dateRange.endDate,
-              }),
-            ...(selectedCampaigns.length > 0 && {
-              campaignIds: selectedCampaigns,
-            }),
-          },
-        }),
-        CreatorAPIInstance.get("getNewsletter", {
-          params: {
-            creatorId: identifier,
-            ...(dateRange.endDate &&
-              dateRange.startDate && {
-                from: dateRange.startDate,
-                to: dateRange.endDate,
-              }),
-            ...(selectedCampaigns.length > 0 && {
-              campaignIds: selectedCampaigns,
-            }),
-          },
-        }),
-      ])
-        .then((res: Array<any>) => {
-          dispatch(setClicked(res[0].data.clicked));
-          dispatch(setCampaign({ campaign: res[0].data.data }));
-          dispatch(setPrevRangeData(res[0].data.prevData));
-          dispatch(setNewsletter(res[1].data));
-        })
-        .finally(() => {
-          dispatch(setCampaignLoading(false));
-        });
-    },
-    [dispatch]
-  );
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+  };
+
+  const renderSection = () => {
+    switch (selectedTab) {
+      case "new":
+        return <NewCampaignRequests />;
+      case "readyToPublish":
+        return <ReadyToPublishCampaigns />;
+      case "active":
+        return <ActiveCampaigns />;
+      case "completed":
+        return <CompletedCampaigns />;
+      default:
+        <></>;
+        break;
+    }
+  };
 
   return (
     <div className="text-left relative pt-1.5 h-full flex flex-col">
       <h1 className="font-semibold font-[Inter] text-[18px] md:text-xl -tracking-[.6px]">
-        Reporting
+        All Campaigns
       </h1>
       <p className="text-[12px] md:text-sm xsm:mt-1.5 font-normal text-secondry1">
-        Here’s a snapshot of your campaigns, all in one place
+        Your one-stop-shop to manage your campaigns from start to finish
       </p>
 
       <div className="flex md:hidden w-full border-[1px] focus:ring-0 border-main mt-4 rounded-[10px] p-2 text-sm font-normal bg-white leading-3.5">
@@ -89,19 +50,57 @@ const CreatorDashboard = () => {
       <motion.div
         className="flex xsm:hidden"
         initial="hidden"
-        animate={`${isFirstLoad && isCampaignLoading ? "" : "show"}`}
+        animate={`${isFirstLoad ? "" : "show"}`}
         variants={MAIN_ROUTE_FADE_UP_ANIMATION_VARIANTS()}
       >
         <div className="flex-1">
-          <CampaignFilter loadCampaignData={loadCampaignData} />
-          {!isCampaignLoading && <CampaignOverView />}
+          <div className="flex justify-between items-center mt-4">
+            <div>
+              <button
+                className={`inline-flex items-center justify-center text-primary text-[14px] font-semibold px-4 py-[10px] font-[Inter] rounded-[10px] sm:w-[170px] me-2 ${
+                  selectedTab === "new"
+                    ? "bg-white border border-solid border-main shadow-md"
+                    : "bg-transparent ring-none"
+                }`}
+                onClick={() => handleTabClick("new")}
+              >
+                New Requests
+              </button>
+              <button
+                className={`inline-flex items-center justify-center text-primary text-[14px] font-semibold px-4 py-[10px] font-[Inter] rounded-[10px] sm:w-[170px] me-2 ${
+                  selectedTab === "readyToPublish"
+                    ? "bg-white border border-solid border-main shadow-md"
+                    : "bg-transparent ring-none"
+                }`}
+                onClick={() => handleTabClick("readyToPublish")}
+              >
+                Ready To Publish
+              </button>
+              <button
+                className={`inline-flex items-center justify-center text-primary text-[14px] font-semibold px-4 py-[10px] font-[Inter] rounded-[10px] sm:w-[170px] me-2 ${
+                  selectedTab === "active"
+                    ? "bg-white border border-solid border-main shadow-md"
+                    : "bg-transparent ring-none"
+                }`}
+                onClick={() => handleTabClick("active")}
+              >
+                Active Campaigns
+              </button>
+              <button
+                className={`inline-flex items-center justify-center text-primary text-[14px] font-semibold px-4 py-[10px] font-[Inter] rounded-[10px] sm:w-[170px] me-2 ${
+                  selectedTab === "completed"
+                    ? "bg-white border border-solid border-main shadow-md"
+                    : "bg-transparent ring-none"
+                }`}
+                onClick={() => handleTabClick("completed")}
+              >
+                Completed
+              </button>
+            </div>
+          </div>
+          {renderSection()}
         </div>
       </motion.div>
-      {isCampaignLoading && (
-        <div className="flex-auto relative">
-          <Loading />
-        </div>
-      )}
     </div>
   );
 };
